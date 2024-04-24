47aa8a9f2b63744170ee7ce00ddd241fa3b54017
f4Lq <167790412+f4Lq@users.noreply.github.com>
2024-04-24 16:12:26 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,11 @@
+DensityMultiplier = 0.08  -- Only change the numbers after (dot(.)). By changing the number, you increase or decrease the number of NPCs. This number is a percentage
+Citizen.CreateThread(function()
+	while true do
+	    Citizen.Wait(0)
+	    SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetPedDensityMultiplierThisFrame(DensityMultiplier)
+	    SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+lua54 'yes'
+
+game 'gta5'
+name 'f4Lq_lowNPC'
+author 'faLq'
+version '1.2'
+
+description 'Using this script you will reduce/increase the number of NPCs on roads/sidewalks'
+
+
+client_scripts {
+    "client.lua",
+}
